5bb735aa564fd77609ee209ec532ac81a7656cae
Kohsmos <52663696+Kohsmos@users.noreply.github.com>
2021-03-03 14:49:47 +0900

    Create LDA.java

---
+++ LDA/java/LDA.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+public class LDA {
+    public void run(List<String> targets, int k, int epochs, double alpha, double beta) {
+        System.out.println(targets);
+        List<String> words = new LinkedList<>();
+        List<List<Integer>> topics = new LinkedList<>();
+        List<int[]> documentTopic = new LinkedList<>();
+        Map<String, Integer>[] topicWord = new Map[k];
+        for (int i=0; i<k; i++) {
+            topicWord[i] = new HashMap<>();
+        }
+        for (int i=0; i<targets.size(); i++) {
+            documentTopic.add(new int[k]);
+            String[] sentenceSplit = targets.get(i).split(" ");
+            topics.add(new LinkedList<>());
+            for (int j=0; j<sentenceSplit.length; j++) {
+                String word = sentenceSplit[j];
+                int randomTopic = (int) (Math.random() * k);
+                topics.get(i).add(randomTopic);
+                if (!words.contains(word)) {
+                    words.add(sentenceSplit[j]);
+                }
+                documentTopic.get(i)[randomTopic]++;
+                for (int l=0; l<k; l++) {
+                    if (l==randomTopic) {
+                        if (topicWord[l].containsKey(word)) {
+                            topicWord[l].put(word, topicWord[l].get(word) + 1);
+                        } else {
+                            topicWord[l].put(word, 1);
+                        }
+                    } else {
+                        if (!topicWord[l].containsKey(word)) {
+                            topicWord[l].put(word, 0);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(topics);
+        System.out.println(topicWord[0]);
+        System.out.println(topicWord[1]);
+
+        for (int epoch=0; epoch<epochs; epoch++) {
+            for (int i=0; i<targets.size(); i++) {
+                String[] sentenceSplit = targets.get(i).split(" ");
+                for (int j=0; j<sentenceSplit.length; j++) {
+                    double max = 0;
+                    int maxIdx = -1;
+                    String word = sentenceSplit[j];
+                    int topic = topics.get(i).get(j);
+                    documentTopic.get(i)[topic]--;
+                    topicWord[topic].put(word,topicWord[topic].get(word)-1);
+                    int documentTopicSum = 0;
+                    for (int l=0; l<k; l++) {
+                        documentTopicSum+=documentTopic.get(i)[l];
+                    }
+                    for (int l=0; l<k; l++) {
+                        int topicWordSum = 0;
+                        for (int m=0; m<words.size(); m++) {
+                            topicWordSum+=topicWord[l].get(words.get(m));
+                        }
+                        double score = ((double)documentTopic.get(i)[l] + alpha/k)/(documentTopicSum+alpha) * ((double)topicWord[l].get(word)+beta)/((double)topicWordSum+beta*words.size());
+                        if (max<=score) {
+                            max = score;
+                            maxIdx = l;
+                        }
+                    }
+                    documentTopic.get(i)[maxIdx]++;
+                    topicWord[maxIdx].put(word,topicWord[maxIdx].get(word)+1);
+                    topics.get(i).set(j,maxIdx);
+                }
+            }
+        }
+
+        List<String>[] resTopic = new List[k];
+        for (int i=0; i<k; i++) {
+            resTopic[i] = new LinkedList<>();
+        }
+
+        for (int i=0; i<topics.size(); i++) {
+            String[] sentenceSplit = targets.get(i).split(" ");
+            for (int j=0; j<topics.get(i).size(); j++) {
+                if (!resTopic[topics.get(i).get(j)].contains(sentenceSplit[j])) {
+                    resTopic[topics.get(i).get(j)].add(sentenceSplit[j]);
+                }
+            }
+        }
+
+        System.out.println(topics);
+        System.out.println(topicWord[0]);
+        System.out.println(topicWord[1]);
+
+        for (int i=0; i<k; i++) {
+            System.out.println(resTopic[i]);
+        }
+
+
+    }
+}
