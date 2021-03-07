741b8ac710e6f529cf1f285dc80a32955b863ff4
Kohsmos <52663696+Kohsmos@users.noreply.github.com>
2021-03-07 21:55:00 +0900

    Update minute_candle.py

--- bitcoin/minute_candle.py
+++ bitcoin/minute_candle.py
@@ -18,3 +18,14 @@ with open("./data.txt", mode = "wt", encoding="utf-8") as f:
             trade_volume = str(r["candle_acc_trade_volume"])
             f.write("\t".join([ust_time, opening, high, low, trade, trade_price, trade_volume])+"\n")
         time.sleep(0.1)
+        
+        
+with open("./data.txt", mode="rt", encoding="utf-8") as f:
+    data = [line.replace("\n","") for line in f.readlines()]
+    
+data.reverse()
+with open("./data2.txt", mode="wt", encoding="utf-8") as f:
+    for d in data:
+        f.write(d+"\n")
+with open("./newest.txt", mode="wt", encoding="utf-8") as f:
+    f.write(data[-1])
